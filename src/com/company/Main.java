2b9fc91ae6dbbcc56743a3a18d8b6fe44b796e42
vincenzo <qwauknation@yahoo.com>
2019-05-03 16:05:30 -0400

    Resume Project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Resume resume = new Resume();
 
-        public class Resume
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        String email;
+        String addEduHistory;
+
+        //ASK USER FOR NAME
+        System.out.print("This is a resume builder, Please enter your name: ");
+
+        name = scanner.nextLine();
+        resume.setName(name);
+
+        //ASK USER FOR EMAIL
+        System.out.print("Please enter your email: ");
+
+        email = scanner.nextLine();
+        resume.setEmailAddress(email);
+
+        //ASK IF USER HAS EDUCATION HISTORY
+        System.out.print("Would you like to provide Educational history? Please enter y(Yes) or n(No): ");
     }
 }
---
+++ src/com/company/Skill.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Skill  {
+    private String skillName;
+    private String competency;
+
+    public Skill(){
+
+    }
+
+    public Skill(String skillName, String competency){
+        this.skillName = skillName;
+        this.competency = competency;
+    }
+}
+
+// Rename file name to Skill
