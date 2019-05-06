55344192fba71bd3bdb6e0a29361ba862f75c4e5
vincenzo <qwauknation@yahoo.com>
2019-05-06 09:40:17 -0400

    Commit

--- src/com/company/Skill.java
+++ src/com/company/Skill.java
@@ -3,10 +3,12 @@ package com.company;
 public class Skill  {
     private String skillName;
     private String competency;
+    private String skillz[];
 
     public Skill(){
 
     }
+
 
     public Skill(String skillName, String competency){
         this.skillName = skillName;
